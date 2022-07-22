3ca4e1c2b0d6197b0aac9247635b3a0bc02e5159
Derek Cooper <derekcooper20@gmail.com>
2022-07-21 19:28:38 -0600

    Adds basic arrays and functions for side menu

---
+++ main.js
@@ -0,0 +1,73 @@
+// QuerySelectors here!
+var newSides = document.querySelector(".random-sides");
+var randomButton = document.querySelector(".show-random");
+
+
+var sides = [
+  "Miso Glazed Carrots",
+  "Coleslaw",
+  "Garden Salad",
+  "Crispy Potatoes",
+  "Sweet Potato Tots",
+  "Coconut Rice",
+  "Caeser Salad",
+  "Shrimp Summer Rolls",
+  "Garlic Butter Mushrooms",
+  "Hush Puppies"
+];
+
+var mains = [
+  "Spaghetti and Meatballs",
+  "Pineapple Chicken",
+  "Shakshuka",
+  "Thai Yellow Curry",
+  "Bibimbap",
+  "Chicken Parmesean",
+  "Butternut Squash Soup",
+  "BBQ Chicken Burgers",
+  "Ramen",
+  "Empanadas",
+  "Chicken Fried Rice",
+  "Sheet Pan Fajitas",
+  "Margarita Pizza"
+];
+var desserts = [
+  "Desserts",
+  "Apple Pie",
+  "Lemon Meringue Pie",
+  "Black Forest Cake",
+  "Banana Bread",
+  "Peach Cobbler",
+  "Cheesecake",
+  "Funfetti Cake",
+  "Baklava",
+  "Flan",
+  "Macarons",
+  "Macaroons",
+  "Chocolate Cupcakes",
+  "Pavlova",
+  "Pumpkin Pie",
+  "Key Lime Pie",
+  "Tart Tatin",
+  "Croissants",
+  "Eclairs"
+]
+
+//Eventlisteners here!
+window.addEventListener("click", getRandomSide(sides));
+randomButton.addEventListener("click", getRandomSide);
+
+
+
+
+
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
+
+function getRandomSide(sides) {
+  var sideIndex = getRandomIndex(sides);
+  newSides.innerHTML = sides[sideIndex];
+}
+
